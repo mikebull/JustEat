972fe5fe8219bd2f4b95e993d3947e6545785ac5
Mike <Michael.Bull@live.uwe.ac.uk>
2015-11-09 22:02:05 +0000

    Add Ninject and set up bindings

---
+++ Source/JustEat/Modules/BusinessLogicModule.cs
@@ -0,0 +1,21 @@
+﻿using JustEat.Factory;
+using JustEat.Factory.Interfaces;
+using JustEat.Service;
+using JustEat.Service.Interfaces;
+using Ninject.Modules;
+
+namespace JustEat.Modules
+{
+    public class BusinessLogicModule : NinjectModule
+    {
+        /// <summary>
+        /// Bindings for Ninject
+        /// </summary>
+        public override void Load()
+        {
+            Bind<IJustEatApiService>().To<JustEatApiService>();
+            Bind<IClientFactory>().To<RestaurantApiClientFactory>();
+            Bind<IRequestFactory>().To<RestaurantApiRequestFactory>();
+        }
+    }
+}
