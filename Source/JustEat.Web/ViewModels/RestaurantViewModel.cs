5ddc27b9f734c528627b5f75af5cfb404235281e
Mike <Michael.Bull@live.uwe.ac.uk>
2015-11-09 22:09:05 +0000

    Add ViewModel for restaurant page

---
+++ Source/JustEat.Web/ViewModels/RestaurantViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using JustEat.Model;
+
+namespace JustEat.Web.ViewModels
+{
+    public class RestaurantViewModel
+    {
+        public IEnumerable<Restaurant> Restaurants { get; set; }
+
+        [Required(ErrorMessage = "Please enter a valid outcode")]
+        public string Outcode { get; set; }
+    }
+}
