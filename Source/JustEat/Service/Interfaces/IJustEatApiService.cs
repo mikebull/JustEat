d40740d657364ecccf9b64e4fb52dc30c0411722
Mike <Michael.Bull@live.uwe.ac.uk>
2015-11-09 21:48:35 +0000

    Add service interface

---
+++ Source/JustEat/Service/Interfaces/IJustEatApiService.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using JustEat.Model;
+
+namespace JustEat.Service.Interfaces
+{
+    public interface IJustEatApiService
+    {
+        IEnumerable<Restaurant> GetRestaurantsByOutcode(string outcode, int page = 1, int pageSize = 5);
+    }
+}
